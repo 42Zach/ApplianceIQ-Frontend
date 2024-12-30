d449fe76778f6bcfc18fb213c8078a6a9cf16737
Zachary Johnson <zachajohnson2004@gmail.com>
2024-12-30 16:21:30 -0700

    Imported REACT_APP_API

--- ApplianceIQ/src/pages/HomePage.jsx
+++ ApplianceIQ/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import ApplianceCard from '../components/ApplianceCard';
 import '../assets/styles/HomePage.css';
 import Navbar from '../components/Navbar';
-import REACT_APP_API from '../.env';
+import REACT_APP_API from '../../.env';
 
 const HomePage = () => {
     const [appliances, setAppliances] = useState([]);
